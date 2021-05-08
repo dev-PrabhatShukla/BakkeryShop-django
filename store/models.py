4dc810cac6946a3b55610ba8236c080a3ba4c6ab
Prabhat Shukla <pracoderma@gmail.com>
2021-05-09 02:44:10 +0530

    store and cart functionality

--- store/models.py
+++ store/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
+    delivery_charge = models.IntegerField()
     images = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
@@ -23,3 +24,32 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+class variationManager(models.Manager):
+    def weights(self):
+        return super(variationManager, self).filter(variation_category='weight', is_active=True)
+
+
+    def special_messages(self):
+        return super(variationManager, self).filter(variation_category='special_message', is_active=True)
+
+
+variation_category_choice = (
+    ('weight', 'weight'),
+    ('special_message', 'special_message'),
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = variationManager()
+
+
+    def __str__(self):
+        return self.variation_value
